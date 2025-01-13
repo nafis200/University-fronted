113b08d86dc14b3eeb9e169b40327e22063688d2
nafis200 <nafisahamed14@gmail.com>
2025-01-13 14:44:45 +0600

    login-user

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,14 +1,25 @@
-import { Layout} from "antd";
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
-const { Header, Content} = Layout;
+import { Button, Layout } from 'antd';
+import Sidebar from './Sidebar';
+import { useAppDispatch } from '../../redux/hooks';
+import { logout } from '../../redux/features/auth/authSlice';
+import { Outlet } from 'react-router-dom';
+const { Header, Content } = Layout;
+
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
-    <Layout style={{ height: "100vh" }}>
-      <Sidebar/>
+    <Layout style={{ height: '100vh' }}>
+      <Sidebar />
       <Layout>
-        <Header style={{ padding: 0 }} />
-        <Content style={{ margin: "24px 16px 0" }}>
+        <Header>
+          <Button onClick={handleLogout}>Logout</Button>{' '}
+        </Header>
+        <Content style={{ margin: '24px 16px 0' }}>
           <div
             style={{
               padding: 24,
@@ -18,9 +29,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        {/* <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer> */}
       </Layout>
     </Layout>
   );
