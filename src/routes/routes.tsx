e4b777baa6f31620f64a93d5dfecf222c540a342
nafis200 <nafisahamed14@gmail.com>
2025-01-16 18:19:20 +0600

    5th branch

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,9 +3,9 @@ import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import { routeGenerator } from "../utils/routesGenerator";
-import { adminPaths } from "./admin.routes";
 import { facultyPaths } from "./faculty.routes";
 import { studentPaths } from "./student.routes";
+import { adminPaths } from "./admin.routes";
 
 
 const router = createBrowserRouter([
