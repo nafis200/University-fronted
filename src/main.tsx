000a60e9aa5295e0207abef4958daf09330406a6
nafis200 <nafisahamed14@gmail.com>
2025-02-02 22:41:33 +0600

    ch

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/routes.tsx';
 import { Provider } from 'react-redux';
